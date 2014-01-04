843603c269f8b2ea28b96d621b82a43a7b9ead26
geoffrowland <rowland.geoff@gmail.com>
2014-01-04 20:58:37 +0000

    Update lib.php

--- lib.php
+++ lib.php
@@ -17,21 +17,23 @@
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Plugin for MathSlate equation editor.
+ * Plugin for ChemSlate equation editor.
  *
  * @package   tinymce_mathslate
- * @copyright 2013 Daniel Thies
+ * @package tinymce_chemslate
+ * @copyright 2014 Geoffrey Rowland
+ * modified from tinymce_mathslate @copyright 2013 Daniel Thies
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class tinymce_mathslate extends editor_tinymce_plugin {
+class tinymce_chemslate extends editor_tinymce_plugin {
     /** @var array list of buttons defined by this plugin */
-    protected $buttons = array('mathslate');
+    protected $buttons = array('chemslate');
 
     protected function update_init_params(array &$params, context $context,
             array $options = null) {
 
         // Add button before 'nonbreaking' in advancedbuttons3.
-        $this->add_button_before($params, 3, 'mathslate', 'nonbreaking');
+        $this->add_button_before($params, 3, 'chemslate', 'nonbreaking');
 
         // Add JS file, which uses default name.
         $this->add_js_plugin($params);
